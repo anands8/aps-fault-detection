77e3c5aa01d8601d05f781abf55c01cd3ae04500
anands8 <anand.s8@outlook.com>
2022-11-27 15:29:06 +0530

    updated requirements

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Anand",
+    author_email="anand.s8@outlook.com",
+    packages = fiind_packages(,)
+    install_requires=get_requirements()
+    
+
+)
